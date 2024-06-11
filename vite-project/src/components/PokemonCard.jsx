d0d3992cfe587d832bc41641f47bb9bf9404e555
Damien GOGUET <goguet.damien@hotmail.fr>
2024-06-11 17:16:20 +0200

    add mew and css

--- vite-project/src/components/PokemonCard.jsx
+++ vite-project/src/components/PokemonCard.jsx
@@ -1,10 +1,42 @@
-function PokemonCard() {
+import React, { useState } from 'react';
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        type: "plant",
+    },
+    {
+        name: "mew",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+        type: "psy",
+    },
+];
+
+const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+const PokemonCard = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const pokemon = pokemonList[currentIndex];
+
+
+    const nextPokemon = () => {
+        setCurrentIndex((currentIndex + 1) % pokemonList.length);
+    };
+
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
+        <div className="pokemon-card">
+            <h1 className="pokemon-name">{capitalizeFirstLetter(pokemon.name)}</h1>
+            {pokemon.imgSrc ? (
+                <img className="pokemon-img" src={pokemon.imgSrc} alt={pokemon.name} />
+            ) : (
+                <p className="pokemon-missing-img">???</p>
+            )}
+            <button onClick={nextPokemon}>Suivant</button>
+        </div>
     );
-}
+};
 
 export default PokemonCard;
