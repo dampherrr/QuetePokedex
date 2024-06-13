08056af1b1d21bb699d830d4b2b300a3ad69f3b6
Damien GOGUET <goguet.damien@hotmail.fr>
2024-06-13 11:59:59 +0200

    Add PropTypes

--- vite-project/src/components/PokemonCard.jsx
+++ vite-project/src/components/PokemonCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const PokemonCard = ({ pokemon }) => {
     return (
@@ -8,5 +9,13 @@ const PokemonCard = ({ pokemon }) => {
         </figure>
     );
 }
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+})
+}
+
 
 export default PokemonCard;
